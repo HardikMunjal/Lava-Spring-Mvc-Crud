347294af4a730a561c6b31a54688235f8ce2d9d4
HardikMunjal <hardik.munjal@polestarllp.com>
2015-01-25 21:56:46 +0530

    used @service,@autowired notion and implemented simple crud using jsp

--- LavaSpringMvc/src/main/java/com/draka/hardi/HomeController.java
+++ LavaSpringMvc/src/main/java/com/draka/hardi/HomeController.java
@@ -1,15 +1,22 @@
 package com.draka.hardi;
 
+import java.io.IOException;
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.draka.dao.ContactDAO;
+import com.draka.model.Contact;
 
 /**
  * Handles requests for the application home page.
@@ -35,5 +42,6 @@ public class HomeController {
 		
 		return "home";
 	}
+	
 	
 }
